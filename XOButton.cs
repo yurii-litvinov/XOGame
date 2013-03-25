def47dd3bcaedc6d3f227f97b5024a4ec2e247bb
yurii-litvinov <yurii.litvinov@gmail.com>
2013-03-26 01:56:25 +0400

    Initial commit for XOGame project.

---
+++ XOButton.cs
@@ -0,0 +1,98 @@
+﻿namespace XOGame
+{
+    using System;
+    using Ubiq.Graphics;
+
+    class XOButton
+    {
+        public enum StateEnum
+        {
+            Empty,
+            X,
+            O
+        }
+
+        public StateEnum State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                var str = "X";
+                switch (value)
+                {
+                    case StateEnum.Empty:
+                        throw new Exception("Can not set button state to Empty");
+                    case StateEnum.O:
+                        str = "O";
+                        break;
+                    case StateEnum.X:
+                        str = "X";
+                        break;
+                }
+                control.Text = str;
+                //owner.ReDrawGraphics();
+            }
+        }
+
+        public VisualElement Control
+        {
+            get
+            {
+                return control;
+            }
+        }
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+
+        public event Action<XOButton> Clicked = (sender) => { };
+
+        public XOButton(int x, int y, MExtendedThreadApp owner)
+        {
+            control = new Button
+            {
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+
+                // VerticalAlignment.Stretch and HorizontalAlignment.Stretch make buttons appear in lower right corner,
+                // instead of stretching buttons to a whole grid cell, so setting Height and Width properties manually.
+                Height = owner.Screen.Height / 3 - 5,
+                Width = owner.Screen.Width / 3 - 5,
+
+                Background = new SolidColorBrush(Colors.DarkGray),
+                Text = " "
+            };
+
+            control.Pressed += (s, e) => Clicked(this);
+            this.x = x;
+            this.y = y;
+            this.owner = owner;
+        }
+
+        private readonly Button control;
+
+        private StateEnum state = StateEnum.Empty;
+
+        private readonly MExtendedThreadApp owner;
+
+        private readonly int x;
+
+        private readonly int y;
+    }
+}
---
+++ XOGameWithButtons.cs
@@ -0,0 +1,69 @@
+﻿namespace XOGame
+{
+    using Ubiq.Graphics;
+    
+    public sealed class XOGameWithButtons: MExtendedThreadApp
+    {
+        protected override void MainOverride()
+        {
+            Screen.Init();
+            
+            const int gridSize = 3;
+            var grid = new Grid(gridSize, gridSize)
+            {
+                Margin = new Thickness(5, 5, 5, 5),
+                VerticalAlignment = VerticalAlignment.Stretch,
+                HorizontalAlignment = HorizontalAlignment.Stretch
+            };
+
+            for (int x = 0; x < 3; ++x)
+            {
+                for (int y = 0; y < 3; ++y)
+                {
+                    var button = new XOButton(x, y, this);
+                    button.Clicked += OnButtonClicked;
+                    grid[x, y] = button.Control;
+                }
+            }
+
+            Screen.Content = grid;
+            Screen.ControlMode = true;
+            
+            WaitForInput("7890\r");            
+        }
+
+        void OnButtonClicked(XOButton button)
+        {
+            if (button.State != XOButton.StateEnum.Empty)
+            {
+                return;
+            }
+
+            switch (turn)
+            {
+                case TurnEnum.X:
+                    button.State = XOButton.StateEnum.X;
+                    turn = TurnEnum.O;
+                    break;
+                case TurnEnum.O:
+                    button.State = XOButton.StateEnum.O;
+                    turn = TurnEnum.X;
+                    break;
+            }
+
+            CheckPosition();
+        }
+
+        private void CheckPosition()
+        {
+        }
+
+        private enum TurnEnum 
+        {
+            X,
+            O
+        }
+
+        private TurnEnum turn = TurnEnum.X;
+    }
+}
